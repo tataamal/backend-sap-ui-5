b17086c99c8fde307b80518ea34418924fc39748
niltal-amal <tataamal1128@gmail.com>
2026-04-13 14:51:00 +0700

    update schedule

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('sap:fetch-sales-orders')->dailyAt('01:00');
+Schedule::command('sap:fetch-sales-orders')->everyThreeHours();
